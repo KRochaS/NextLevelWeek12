5b839dd0f2bb1a6940825aa9528ce4362e87c4f3
KRochaS <karinerocha.s@hotmail.com>
2023-05-21 00:06:15 -0300

    server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,30 @@
-import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import fastify from 'fastify'
+import { authRoutes } from './routes/auth'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-  return users
+app.register(cors, {
+  origin: true, // todas as URL`s de front poderão acessar o backend
+  // origin: ['http://localhost:3333', 'https://rocketseat.com.br']
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+// registrar um arquivo separado
+app.register(authRoutes)
+app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('HTTP server running on http://localhost:3333')
