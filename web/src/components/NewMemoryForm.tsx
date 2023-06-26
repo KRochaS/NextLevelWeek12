d0a562c6b8132c9616edfa899a36bbef0e4d5ac5
KRochaS <karinerocha.s@hotmail.com>
2023-06-26 19:44:21 -0300

    language

--- web/src/components/NewMemoryForm.tsx
+++ web/src/components/NewMemoryForm.tsx
@@ -90,14 +90,14 @@ export function NewMemoryForm() {
         name="content"
         spellCheck={false}
         className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
-        placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
+        placeholder="Feel free to add photos and stories about that experience"
       />
 
       <button
         className="inline-block  self-end rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
         type="submit"
       >
-        Salvar
+        Save
       </button>
     </form>
   )
