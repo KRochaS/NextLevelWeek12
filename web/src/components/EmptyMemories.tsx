4504444eecb3d1838915d998ca2be1ef56630482
KRochaS <karinerocha.s@hotmail.com>
2023-07-09 19:49:52 -0300

    web

--- web/src/components/EmptyMemories.tsx
+++ web/src/components/EmptyMemories.tsx
@@ -2,10 +2,10 @@ export function EmptyMemories() {
   return (
     <div className="flex flex-1 items-center justify-center p-16">
       <p className="w-[360px] text-center leading-relaxed">
-        Você ainda não registrou nenhuma lembrança, comece a{' '}
+        You haven't recorded any memories yet, start{' '}
         <a href="#" className="underline hover:text-gray-50">
           {' '}
-          criar agora
+          creating them now
         </a>
         !
       </p>
